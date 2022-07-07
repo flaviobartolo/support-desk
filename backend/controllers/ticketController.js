62763ac7f496a00f9e803d8e13a024a5104c878a
flaviobartolo <flavio.bartolo2@gmail.com>
2022-07-07 15:20:09 +0100

    Single Ticket, Update& Delete

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -2,6 +2,7 @@ const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 const Ticket = require('../models/ticketModel')
 
+
 // @desc get all the tickets from currently logged user
 // route GET /api/tickets
 // @access Private 
@@ -12,6 +13,56 @@ const getTickets = asyncHandler( async (req, res) => {
 
     res.status(200).json(tickets)
 })
+
+
+// @desc get a ticket by its ID
+// route GET /api/tickets/:id
+// @access Private 
+const getTicket = asyncHandler( async (req, res) => {
+
+    const ticket = await Ticket.findOne({_id: req.params.id, user: req.user.id})
+
+    if (!ticket){
+        res.status(404)
+        throw new Error('Ticket doesnt exist or doesnt belong to the logged user')
+    }
+
+    res.status(200).json(ticket)
+
+})
+
+
+// @desc delete a ticket by its ID
+// route DELETE /api/tickets/:id
+// @access Private
+const deleteTicket = asyncHandler( async (req, res) => {
+    const ticket = await Ticket.findOneAndDelete({_id: req.params.id, user: req.user.id})
+    
+    if (!ticket){
+        res.status(404)
+        throw new Error('Ticket doesnt exist or doesnt belong to the logged user')
+    }
+
+    res.status(200).json({message: 'Ticket deleted successfully', success: true})
+
+})
+
+
+// @desc update a ticket by its ID
+// route PUT /api/tickets/:id
+// @access Private
+const updateTicket = asyncHandler( async (req, res) => {
+    const ticket = await Ticket.findOneAndUpdate({_id: req.params.id, user: req.user.id, }, req.body, {new: true})
+
+    if (!ticket){
+        res.status(404)
+        throw new Error('Ticket doesnt exist or doesnt belong to the logged user')
+    }
+
+    res.status(200).json(ticket)
+
+})
+
 
 // @desc create a ticket for the currently logged user
 // route POST /api/tickets
@@ -35,7 +86,11 @@ const createTicket = asyncHandler( async (req, res) => {
     res.status(201).json(ticket)
 })
 
+
 module.exports = {
     getTickets,
-    createTicket
+    getTicket,
+    createTicket,
+    deleteTicket,
+    updateTicket
 }
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const {protect} = require('../middleware/authMiddleware')
-const {getTickets, createTicket} = require('../controllers/ticketController')
+const {getTickets, getTicket, createTicket, deleteTicket, updateTicket} = require('../controllers/ticketController')
 
 // .route allows you to chain multiple different types of requests - ex: .get().post()
 router.route('/').get(protect, getTickets).post(protect, createTicket)
+router.route('/:id').get(protect, getTicket).delete(protect, deleteTicket).put(protect, updateTicket)
 
 module.exports = router
