81ec3c73a0f7705e22e6076f323993a70819a7d2
flaviobartolo <flavio.bartolo2@gmail.com>
2022-07-08 15:10:50 +0100

    add tickets to redux and create tickets functionality

---
+++ frontend/src/features/tickets/ticketService.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const API_URL = '/api/tickets'
+
+// Create a new ticket
+const create = async (ticketData, token) => {
+
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+    const response = await axios.post(API_URL, ticketData, config)
+
+    return response.data
+}
+
+
+const ticketService = {
+    create,
+}
+
+export default ticketService
